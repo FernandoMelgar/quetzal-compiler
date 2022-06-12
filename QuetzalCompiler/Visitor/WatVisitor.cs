d25a5bdebca6bf08dd7d230b8009693dc206785f
Alex Serrano <a01376364@itesm.mx>
2022-06-11 19:47:27 -0500

    WIP: Visit de Function, ParamList, DefList y VarDefList en WatVisitor.cs

--- QuetzalCompiler/Visitor/WatVisitor.cs
+++ QuetzalCompiler/Visitor/WatVisitor.cs
@@ -1,11 +1,78 @@
+using System.Text;
+
 namespace QuetzalCompiler.Visitor;
 
 public class WatVisitor
 {
-    HashSet<string> table;
-
-    public WatVisitor(HashSet<string> table)
+    IDictionary<string, ParamsFGST> table;
+    public string CurrentFunction;
+    
+    public WatVisitor(IDictionary<string, ParamsFGST> table)
     {
         this.table = table;
     }
+
+    public string Visit(Function node)
+    {
+        CurrentFunction = node[0].AnchorToken.Lexeme;
+        var paramList = node[1];
+        Visit((dynamic) paramList);
+        var varDefList = node[2];
+
+        var sb = new StringBuilder(); 
+        sb.Append($"  (func\n" + 
+                  $"    (export \"{CurrentFunction}\")\n" + 
+                  Visit((dynamic) paramList) +              // Param List
+                  $"    (result i32)\n" + 
+                  Visit((dynamic) varDefList) +             // VarDefList
+                  // Visit((dynamic) node[3]) +                // Stmt List
+                  $"    i32.const 0\n" + 
+                  $"  )\n");
+
+        return sb.ToString();
+    }
+    
+    public string Visit(ParamList node)
+    {
+        var sb = new StringBuilder();
+        foreach (var n in node) {
+            sb.Append($"    (param ${n.AnchorToken.Lexeme} i32)\n");
+        }
+        return sb.ToString();
+    }
+    
+    public string Visit(VarDefList node)
+    {
+        var sb = new StringBuilder();
+        var localTable = table[CurrentFunction].refLST;
+        foreach (var varName in localTable)
+        {
+            sb.Append($"    (local ${varName} i32)\n");
+        }
+        return sb.ToString();
+    }
+    
+    public string Visit(DefList node)
+    {
+        var sb = new StringBuilder();
+        
+        foreach (var child in node)
+        {
+            if (child.GetType().Name == "VarDef")
+                continue;
+            sb.Append(Visit((dynamic)child));
+        }
+
+        return sb.ToString();
+    }
+
+    public string VisitChildren(Node node)
+    {
+        var sb = new StringBuilder();
+        foreach (var n in node) {
+            sb.Append(Visit((dynamic) n));
+        }
+        return sb.ToString();
+    }
+
 }
--- Tests/WatVisitorTest.cs
+++ Tests/WatVisitorTest.cs
@@ -9,7 +9,7 @@ public class WatVisitorTest
     private readonly TokenClassifier _classifier = new();
 
     [Test]
-    public void TestCreateInstanceWatVisitor()
+    public void TestFunctionWithVarDef()
     {
         var program = @"main() {
             var x;
@@ -20,8 +20,75 @@ public class WatVisitorTest
         fpv.Visit((dynamic) ast);
         var spv = new SecondPassVisitor(fpv.FGST, fpv.VGST);
         spv.Visit((dynamic) ast);
-        var watVisitor = new WatVisitor(spv.VGST);
-        string result = watVisitor.Visit(ast); // Function, Var declaration
-        Assert.True(result.Contains("(local $x i32)"));
+        var watVisitor = new WatVisitor(spv.FGST);
+        string result = watVisitor.Visit((dynamic) ast); // Function, Var declaration
+        Assert.True(result.Contains(@"  (func"));
+        Assert.True(result.Contains(@"    (export ""main"""));
+        Assert.True(result.Contains(@"    (result i32)"));
+        Assert.True(result.Contains(@"    (local $x i32)"));
+        Assert.True(result.Contains(@"    i32.const 0"));
     }
+    
+    [Test]
+    public void TestFunctionParams()
+    {
+        var program = @"sqr(x){}
+            main() {
+            var x;
+        }";
+        var parser = new Parser(_classifier.ClassifyAsEnumerable(program).GetEnumerator());
+        var ast = parser.Program();
+        var fpv = new FirstPassVisitor();
+        fpv.Visit((dynamic) ast);
+        var spv = new SecondPassVisitor(fpv.FGST, fpv.VGST);
+        spv.Visit((dynamic) ast);
+        var watVisitor = new WatVisitor(spv.FGST);
+        string result = watVisitor.Visit((dynamic) ast); // Function, Var declaration
+        Assert.True(result.Contains(@"  (func"));
+        Assert.True(result.Contains(@"    (export ""sqr"""));
+        Assert.True(result.Contains(@"    (result i32)"));
+        Assert.True(result.Contains(@"    (param $x i32)"));
+        Assert.True(result.Contains(@"    i32.const 0"));
+    }
+    
+    [Test]
+    public void TestFunctionMainWithExportTag()
+    {
+        var program = @"main() {
+        }";
+        var parser = new Parser(_classifier.ClassifyAsEnumerable(program).GetEnumerator());
+        var ast = parser.Program();
+        var fpv = new FirstPassVisitor();
+        fpv.Visit((dynamic) ast);
+        var spv = new SecondPassVisitor(fpv.FGST, fpv.VGST);
+        spv.Visit((dynamic) ast);
+        var watVisitor = new WatVisitor(spv.FGST);
+        string result = watVisitor.Visit((dynamic) ast); // Function, Var declaration
+        Assert.True(result.Contains(@"  (func"));
+        Assert.True(result.Contains(@"    (export ""main"""));
+        Assert.True(result.Contains(@"    (result i32)"));
+        Assert.True(result.Contains(@"    i32.const 0"));
+    }
+    
+    [Test]
+    public void TestLocalFunctionSignature()
+    {
+        var program = @"sqr(x){}
+        main() {
+        }";
+        var parser = new Parser(_classifier.ClassifyAsEnumerable(program).GetEnumerator());
+        var ast = parser.Program();
+        var fpv = new FirstPassVisitor();
+        fpv.Visit((dynamic) ast);
+        var spv = new SecondPassVisitor(fpv.FGST, fpv.VGST);
+        spv.Visit((dynamic) ast);
+        var watVisitor = new WatVisitor(spv.FGST);
+        string result = watVisitor.Visit((dynamic) ast); // Function, Var declaration
+        Assert.True(result.Contains(@"  (func $sqr"));
+        Assert.True(result.Contains(@"    (param $x i32)"));
+        Assert.True(result.Contains(@"    (export ""main"""));
+        Assert.True(result.Contains(@"    (result i32)"));
+        Assert.True(result.Contains(@"    i32.const 0"));
+    }
+    
 }
